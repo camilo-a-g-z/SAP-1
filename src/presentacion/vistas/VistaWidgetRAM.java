ae088d00d4a12c04ef6cf67c1c5e9499f33a1328
camilo-a-g-z <cagarciaz@correo.udistrital.edu.co>
2022-08-15 17:54:51 -0500

    Some changes

--- src/presentacion/vistas/VistaWidgetRAM.java
+++ src/presentacion/vistas/VistaWidgetRAM.java
@@ -38,7 +38,7 @@ public class VistaWidgetRAM extends JPanel {
 
     // Constantes
     private static final Dimension buttonSize = new Dimension(22, 22);
-    private static final Dimension WIDGET_SIZE = new Dimension(220, 550);
+    private static final Dimension WIDGET_SIZE = new Dimension(240, 750);
     public static final Color COLOR_ON = new Color(246, 203, 225);
     public static final Color COLOR_OFF = new Color(246, 213, 203);
     public static final Color COLOR_MAR = Color.gray;
